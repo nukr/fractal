1a2165d5b6934f71f4b0bfdc8750af9ae5999396
Nukr <nukrs.w@gmail.com>
2016-07-04 21:24:23 +0800

    sierpinski triangle done

--- src/sierpinski_triangle.js
+++ src/sierpinski_triangle.js
@@ -2,13 +2,13 @@ function sierpinski_triangle (height) {
   var canvas = document.getElementById('triangle')
   var context = canvas.getContext('2d')
 
-  let bottom = Math.round(height * 1.732 / 3 * 2)
+  let edge = Math.round(height * 1.732 / 3 * 2)
 
   context.beginPath()
   // draw triangle
-  let top = [bottom / 2, 0]
+  let top = [edge / 2, 0]
   let left = [0, height]
-  let right = [bottom, height]
+  let right = [edge, height]
 
   context.moveTo(...top)
   context.lineTo(...left)
@@ -16,37 +16,38 @@ function sierpinski_triangle (height) {
   context.lineTo(...top)
   context.stroke()
 
-  function draw_upside_down_triangle (top, height, edge) {
+  function draw_reverse_triangle (top, height, edge, depth) {
     let [top_x, top_y] = top
 
     let next_top = [top_x, top_y + height]
     let next_left = [top_x - edge / 4, top_y + height / 2]
     let next_right = [top_x + edge / 4, top_y + height / 2]
-    console.log(left)
     context.moveTo(...next_top)
     context.lineTo(...next_left)
     context.lineTo(...next_right)
     context.lineTo(...next_top)
     context.stroke()
+
+    if (depth !== 0) {
+      setTimeout(() => {
+        draw_reverse_triangle(top, height / 2, edge / 2, depth - 1)
+      }, 1000)
+    }
+    if (depth !== 0) {
+      setTimeout(() => {
+        draw_reverse_triangle(next_left, height / 2, edge / 2, depth - 1)
+      }, 1000)
+    }
+    if (depth !== 0) {
+      setTimeout(() => {
+        draw_reverse_triangle(next_right, height / 2, edge / 2, depth - 1)
+      }, 1000)
+    }
   }
 
-  setTimeout(() => {
-    draw_upside_down_triangle(top, height, bottom)
-  }, 1000)
-
-  setTimeout(() => {
-    draw_upside_down_triangle(top, height / 2, bottom / 2)
-  }, 2000)
-
-  setTimeout(() => {
-    draw_upside_down_triangle([top[0] - bottom / 4, top[1] + height / 2], height / 2, bottom / 2)
-  }, 2000)
-
-  setTimeout(() => {
-    draw_upside_down_triangle([top[0] + bottom / 4, top[1] + height / 2], height / 2, bottom / 2)
-  }, 2000)
+  draw_reverse_triangle(top, height, edge, 6)
 }
 
 window.onload = function () {
-  sierpinski_triangle(300)
+  sierpinski_triangle(400)
 }
